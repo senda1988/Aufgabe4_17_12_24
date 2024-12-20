7b13e3e2db5221e9a8a3c58f752c9ecb71178975
senda1988 <zidi.senda@gmail.com>
2024-12-20 14:09:44 +0100

    update *

--- Beispiel.py
+++ Beispiel.py
@@ -1,31 +1,36 @@
-from datetime import datetime, date
+# from datetime import datetime, date
 
 
-def tage_bis_datum():
-    date_entry = input("Bitte ein Datum im Format TT.MM.JJJJ eingeben: ")
-    datum = datetime.strptime(date_entry, "%d.%m.%Y").date()
-    # aktuelle datum
-    datum_heute = datetime.today().date()
-    difference = datum - datum_heute
-    return print(f"difference: {difference.days}")
+# def tage_bis_datum():
+#     date_entry = input("Bitte ein Datum im Format TT.MM.JJJJ eingeben: ")
+#     datum = datetime.strptime(date_entry, "%d.%m.%Y").date()
+#     # aktuelle datum
+#     datum_heute = datetime.today().date()
+#     difference = datum - datum_heute
+#     return print(f"difference: {difference.days}")
 
 
-tage_bis_datum()
+# tage_bis_datum()
 
 
-def wochentag_berechnen():
-    datum_entry = input("Bitte ein Datum im Format TT.MM.JJJJ eingeben: ")
-    datum = datetime.strptime(datum_entry, "%d.%m.%Y").date()
-    wochentag = [
-        "Montag",
-        "Dienstag",
-        "Mittwoch",
-        "Donnerstag",
-        "Freitag",
-        "Samstag",
-        "Sonntag",
-    ]
-    return wochentag[datum.weekday()]
+# def wochentag_berechnen():
+#     datum_entry = input("Bitte ein Datum im Format TT.MM.JJJJ eingeben: ")
+#     datum = datetime.strptime(datum_entry, "%d.%m.%Y").date()
+#     wochentag = [
+#         "Montag",
+#         "Dienstag",
+#         "Mittwoch",
+#         "Donnerstag",
+#         "Freitag",
+#         "Samstag",
+#         "Sonntag",
+#     ]
+#     return wochentag[datum.weekday()]
 
 
-print(f" Der Wochentag ist {wochentag_berechnen()}")
+# print(f" Der Wochentag ist {wochentag_berechnen()}")
+
+
+zahlen = [1, 2, 3, 4, 5]
+print("Die erste Zahl ist:", zahlen[0])
+print("Die letzte Zahl ist:", zahlen[-1])
